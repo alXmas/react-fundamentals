fedf069bcbd333f12c79610d9dc88653dfb43c9d
alXmas <alexsandr.xmas@gmail.com>
2020-10-13 10:01:11 +0400

    resolve ex7-extra-1

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -1,18 +1,31 @@
 // Rendering Lists
-// http://localhost:3000/isolated/exercise/07.js
+// http://localhost:3000/isolated/final/07.js
 
 import React from 'react'
 
 const allItems = [
-  {id: 'a', value: 'apple'},
-  {id: 'o', value: 'orange'},
-  {id: 'g', value: 'grape'},
-  {id: 'p', value: 'pear'},
-]
+  { id: 'a', value: 'apple' },
+  { id: 'o', value: 'orange' },
+  { id: 'g', value: 'grape' },
+  { id: 'p', value: 'pear' }
+];
 
 function App() {
-  const [items, setItems] = React.useState(allItems)
+  const [items, setItems] = React.useState([
+    { id: 'a', value: 'apple' },
+    { id: 'o', value: 'orange' },
+    { id: 'g', value: 'grape' },
+    { id: 'p', value: 'pear' }
+  ]);
 
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setItems(shuffle(items));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  
   function addItem() {
     setItems([...items, allItems.find(i => !items.includes(i))])
   }
@@ -36,8 +49,7 @@ function App() {
       </button>
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
         {items.map(item => (
-          // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={item.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.value}-input`}>{item.value}</label>{' '}
             <input id={`${item.value}-input`} defaultValue={item.value} />
@@ -46,6 +58,21 @@ function App() {
       </ul>
     </div>
   )
+  function shuffle(originalArray) {
+    const array = [...originalArray];
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
 }
 
 export default App
+
